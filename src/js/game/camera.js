f3fc27ad5feae7231e1f61a09f028496208164fe
Dimava <dimava2@ya.ru>
2020-05-30 10:56:23 +0300

    use MouseEvent in keyboard hooks

--- src/js/game/camera.js
+++ src/js/game/camera.js
@@ -473,6 +473,8 @@ export class Camera extends BasicSerializableObject {
             this.downPreHandler.dispatch(new Vector(event.clientX, event.clientY), enumMouseButton.middle);
         } else if (event.button === 2) {
             this.downPreHandler.dispatch(new Vector(event.clientX, event.clientY), enumMouseButton.right);
+        } else {
+            this.root.app.inputMgr.handleKeydown(event);
         }
         return false;
     }
@@ -514,6 +516,9 @@ export class Camera extends BasicSerializableObject {
 
         if (!this.checkPreventDoubleMouse()) {
             return;
+        }
+        if (event.button >= 3) {
+            this.root.app.inputMgr.handleKeyup(event);
         }
 
         this.combinedSingleTouchStopHandler(event.clientX, event.clientY);
