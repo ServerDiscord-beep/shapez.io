edca32d23f059814f3f48c9db6b6825727a63f79
Dimava <dimava2@ya.ru>
2020-06-02 19:15:16 +0300

    thinner shape lines

--- src/js/game/shape_definition.js
+++ src/js/game/shape_definition.js
@@ -297,7 +297,7 @@ export class ShapeDefinition extends BasicSerializableObject {
 
                 context.fillStyle = enumColorsToHexCode[color];
                 context.strokeStyle = THEME.items.outline;
-                context.lineWidth = THEME.items.outlineWidth;
+                context.lineWidth = THEME.items.outlineWidth * Math.pow(0.8, layerIndex);
 
                 const insetPadding = 0.0;
 
