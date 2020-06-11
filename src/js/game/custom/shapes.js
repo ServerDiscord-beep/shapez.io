7c277dfc02d15eb084c813b51f5302923cc63811
Dimava <dimava2@ya.ru>
2020-06-12 00:36:39 +0300

    lint-autofix

--- src/js/game/custom/shapes.js
+++ src/js/game/custom/shapes.js
@@ -33,9 +33,9 @@ let customDefaults = {
     maxQuarters: 4,
     minDistance: 6,
     minChance: 4,
-    distChance: 1/3,
+    distChance: 1 / 3,
     maxChance: 12,
-}
+};
 
 registerCustomShape({
     id: "clover",
@@ -47,8 +47,8 @@ registerCustomShape({
         const inner = 0.5;
         const inner_center = 0.45;
         context.lineTo(0, inner);
-        context.bezierCurveTo(0, 1,  inner, 1,  inner_center, inner_center);
-        context.bezierCurveTo(1, inner,  1, 0,  inner, 0);
+        context.bezierCurveTo(0, 1, inner, 1, inner_center, inner_center);
+        context.bezierCurveTo(1, inner, 1, 0, inner, 0);
     },
 });
 
@@ -62,7 +62,7 @@ registerCustomShape({
         context.lineTo(0, inner);
         context.lineTo(Math.sin(Math.PI / 8), Math.cos(Math.PI / 8));
         context.lineTo(inner * Math.sin(Math.PI / 4), inner * Math.cos(Math.PI / 4));
-        context.lineTo(Math.sin(Math.PI * 3 / 8), Math.cos(Math.PI * 3 / 8));
+        context.lineTo(Math.sin((Math.PI * 3) / 8), Math.cos((Math.PI * 3) / 8));
         context.lineTo(inner, 0);
     },
 });
@@ -75,8 +75,8 @@ registerCustomShape({
         begin({ size: 1.2, path: true, zero: true });
         const rad = 0.001;
         // with rounded borders
-        context.arcTo(0, 1,  1, 0,  rad);
-        context.arcTo(1, 0,  0, 0,  rad);
+        context.arcTo(0, 1, 1, 0, rad);
+        context.arcTo(1, 0, 0, 0, rad);
     },
 });
 
@@ -103,11 +103,25 @@ registerCustomShape({
         begin({ size: 1.1, path: true, zero: true });
         const inner = 0.5;
         context.lineTo(inner, 0);
-        context.bezierCurveTo(inner, 0.3,  1, 0.3,  1, 0);
-        context.bezierCurveTo(1, inner,  inner * Math.SQRT2 * 0.9, inner * Math.SQRT2 * 0.9,  inner * Math.SQRT1_2, inner * Math.SQRT1_2);
+        context.bezierCurveTo(inner, 0.3, 1, 0.3, 1, 0);
+        context.bezierCurveTo(
+            1,
+            inner,
+            inner * Math.SQRT2 * 0.9,
+            inner * Math.SQRT2 * 0.9,
+            inner * Math.SQRT1_2,
+            inner * Math.SQRT1_2
+        );
         context.rotate(Math.PI / 4);
-        context.bezierCurveTo(inner, 0.3,  1, 0.3,  1, 0);
-        context.bezierCurveTo(1, inner,  inner * Math.SQRT2 * 0.9, inner * Math.SQRT2 * 0.9,  inner * Math.SQRT1_2, inner * Math.SQRT1_2);
+        context.bezierCurveTo(inner, 0.3, 1, 0.3, 1, 0);
+        context.bezierCurveTo(
+            1,
+            inner,
+            inner * Math.SQRT2 * 0.9,
+            inner * Math.SQRT2 * 0.9,
+            inner * Math.SQRT1_2,
+            inner * Math.SQRT1_2
+        );
     },
 });
 
@@ -119,7 +133,7 @@ registerCustomShape({
     draw({ dims, innerDims, layer, quad, context, color, begin }) {
         begin({ size: 1.3, path: true, zero: true });
         const PI = Math.PI;
-        const PI3 = (PI * 3) / 8 * 0.75;
+        const PI3 = ((PI * 3) / 8) * 0.75;
         const c = 1 / Math.cos(Math.PI / 8);
         const b = c * Math.sin(Math.PI / 8);
 
