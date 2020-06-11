3f79c49d8f142df505338a88e881f62102aa692c
Dimava <dimava2@ya.ru>
2020-06-12 00:34:25 +0300

    add black color and 6 custom shapes

--- src/js/game/custom/shapes.js
+++ src/js/game/custom/shapes.js
@@ -27,22 +27,108 @@ export function registerCustomShape(shapeData) {
     customShapes.push(shapeData);
 }
 
-// registerCustomShape({
-//     id: "clover",
-//     code: "L",
-//     spawnable: true,
-//     spawnColor: "green",
-//     maxQuarters: 4,
-//     minDistance: 6,
-//     minChance: 4,
-//     distChance: 1/3,
-//     maxChance: 12,
-//     draw({ dims, innerDims, layer, quad, context, color, begin }) {
-//         begin({ size: 1.3, path: true, zero: true });
-//         const inner = 0.5;
-//         const inner_center = 0.45;
-//         context.lineTo(0, inner);
-//         context.bezierCurveTo(0, 1,  inner, 1,  inner_center, inner_center);
-//         context.bezierCurveTo(1, inner,  1, 0,  inner, 0);
-//     },
-// });
+let customDefaults = {
+    spawnable: true,
+    spawnColor: "uncolored",
+    maxQuarters: 4,
+    minDistance: 6,
+    minChance: 4,
+    distChance: 1/3,
+    maxChance: 12,
+}
+
+registerCustomShape({
+    id: "clover",
+    code: "L",
+    ...customDefaults,
+    spawnColor: "green",
+    draw({ dims, innerDims, layer, quad, context, color, begin }) {
+        begin({ size: 1.3, path: true, zero: true });
+        const inner = 0.5;
+        const inner_center = 0.45;
+        context.lineTo(0, inner);
+        context.bezierCurveTo(0, 1,  inner, 1,  inner_center, inner_center);
+        context.bezierCurveTo(1, inner,  1, 0,  inner, 0);
+    },
+});
+
+registerCustomShape({
+    id: "star8",
+    code: "T",
+    ...customDefaults,
+    draw({ dims, innerDims, layer, quad, context, color, begin }) {
+        begin({ size: 1.22, path: true, zero: true });
+        const inner = 0.5;
+        context.lineTo(0, inner);
+        context.lineTo(Math.sin(Math.PI / 8), Math.cos(Math.PI / 8));
+        context.lineTo(inner * Math.sin(Math.PI / 4), inner * Math.cos(Math.PI / 4));
+        context.lineTo(Math.sin(Math.PI * 3 / 8), Math.cos(Math.PI * 3 / 8));
+        context.lineTo(inner, 0);
+    },
+});
+
+registerCustomShape({
+    id: "rhombus",
+    code: "B",
+    ...customDefaults,
+    draw({ dims, innerDims, layer, quad, context, color, begin }) {
+        begin({ size: 1.2, path: true, zero: true });
+        const rad = 0.001;
+        // with rounded borders
+        context.arcTo(0, 1,  1, 0,  rad);
+        context.arcTo(1, 0,  0, 0,  rad);
+    },
+});
+
+registerCustomShape({
+    id: "plus",
+    code: "P",
+    ...customDefaults,
+    draw({ dims, innerDims, layer, quad, context, color, begin }) {
+        begin({ size: 1.2, path: true, zero: true });
+        const inner = 0.4;
+        context.lineTo(1, 0);
+        context.lineTo(1, inner);
+        context.lineTo(inner, inner);
+        context.lineTo(inner, 1);
+        context.lineTo(0, 1);
+    },
+});
+
+registerCustomShape({
+    id: "razor",
+    code: "Z",
+    ...customDefaults,
+    draw({ dims, innerDims, layer, quad, context, color, begin }) {
+        begin({ size: 1.1, path: true, zero: true });
+        const inner = 0.5;
+        context.lineTo(inner, 0);
+        context.bezierCurveTo(inner, 0.3,  1, 0.3,  1, 0);
+        context.bezierCurveTo(1, inner,  inner * Math.SQRT2 * 0.9, inner * Math.SQRT2 * 0.9,  inner * Math.SQRT1_2, inner * Math.SQRT1_2);
+        context.rotate(Math.PI / 4);
+        context.bezierCurveTo(inner, 0.3,  1, 0.3,  1, 0);
+        context.bezierCurveTo(1, inner,  inner * Math.SQRT2 * 0.9, inner * Math.SQRT2 * 0.9,  inner * Math.SQRT1_2, inner * Math.SQRT1_2);
+    },
+});
+
+registerCustomShape({
+    id: "sun",
+    code: "U",
+    ...customDefaults,
+    spawnColor: "yellow",
+    draw({ dims, innerDims, layer, quad, context, color, begin }) {
+        begin({ size: 1.3, path: true, zero: true });
+        const PI = Math.PI;
+        const PI3 = (PI * 3) / 8 * 0.75;
+        const c = 1 / Math.cos(Math.PI / 8);
+        const b = c * Math.sin(Math.PI / 8);
+
+        context.moveTo(0, 0);
+        context.rotate(Math.PI / 2);
+        context.arc(c, 0, b, -PI, -PI + PI3);
+        context.rotate(-Math.PI / 4);
+        context.arc(c, 0, b, -PI - PI3, -PI + PI3);
+        context.rotate(-Math.PI / 4);
+        context.arc(c, 0, b, PI - PI3, PI);
+    },
+});
